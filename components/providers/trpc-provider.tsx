6d4308ebd7946603796cbd43fb4d0d981cd37319
Miljan Gicic <miljancode@gmail.com>
2023-08-31 16:16:08 +0200

    fix: minor type errors

--- components/providers/trpc-provider.tsx
+++ components/providers/trpc-provider.tsx
@@ -3,8 +3,10 @@
 import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
+import superjson from "superjson";
 
 import { trpc } from "@/lib/trpc/client";
+import { getBaseUrl } from "@/lib/utils";
 
 export function TrpcProvider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient({}));
@@ -12,9 +14,10 @@ export function TrpcProvider({ children }: { children: React.ReactNode }) {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
+          url: `${getBaseUrl()}/api/trpc`,
         }),
       ],
+      transformer: superjson,
     }),
   );
   return (
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,14 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getBaseUrl() {
+  if (typeof window !== undefined) return "";
+
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+
+  return "http://localhost:3000";
 }
