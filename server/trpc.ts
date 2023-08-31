0e8070931599485ba8bfbf5c49922d836b438cbe
Miljan Gicic <miljancode@gmail.com>
2023-08-31 15:45:03 +0200

    feat: trpc ctx

--- server/trpc.ts
+++ server/trpc.ts
@@ -1,6 +1,22 @@
-import { initTRPC } from "@trpc/server";
+import { initTRPC, type inferAsyncReturnType } from "@trpc/server";
+import { getServerSession } from "next-auth";
+import superjson from "superjson";
 
-const t = initTRPC.create();
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { db } from "@/db";
+
+export const createContext = async () => {
+  const session = await getServerSession(authOptions);
+
+  return {
+    session,
+    db,
+  };
+};
+
+export type Context = inferAsyncReturnType<typeof createContext>;
+
+const t = initTRPC.context<Context>().create({ transformer: superjson });
 
 export const router = t.router;
 export const publicProcedure = t.procedure;
